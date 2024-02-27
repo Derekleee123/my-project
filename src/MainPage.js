78207be6e12ca7b1d7fdeb254dd22bc98f4aebf4
Derek Liang <f80673@gmail.com>
2024-02-27 21:53:32 +0800

    remove unused file, intall antd

---
+++ src/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd';
+
+
+function MainPage() {
+  return (
+    <div><Button type="primary">Primary Button</Button></div>
+  )
+}
+
+export default MainPage
